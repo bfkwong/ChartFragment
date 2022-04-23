1d7d26e9ad8124e85b807fce167a5daa764fb434
Bryan Kwong <bryank458@gmail.com>
2022-04-23 15:46:03 -0700

    fix: finish

--- app/src/main/java/com/bkwongsjsu/chartfragment/MainActivity.java
+++ app/src/main/java/com/bkwongsjsu/chartfragment/MainActivity.java
@@ -4,11 +4,32 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<Float> inner = new ArrayList<>();
+        inner.add(1f);
+        inner.add(2f);
+        List<Float> inneralt = new ArrayList<>();
+        inneralt.add(1f);
+        inneralt.add(20f);
+        List<List<Float>> inner2 = new ArrayList<>();
+        inner2.add(inner);
+        inner2.add(inneralt);
+        List<List<List<Float>>> data = new ArrayList<>();
+        data.add(inner2);
+
+        ChartFragment fragment = new ChartFragment(data);
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.chart_fragment_container, fragment)
+                .commit();
     }
 }
